ffb1f466dcd1baa096f2bf7efbcfc08bc1d02cf8
Ricardo Viloria <34728899+Rvilov@users.noreply.github.com>
2024-08-17 02:29:31 +0000

    Home listo pero falta el responsive, se crean archivos para el sistema de inicio 2

---
+++ frontend/src/components/login-sistema/login.jsx
@@ -0,0 +1,16 @@
+import "../../styles/login-sistema/login";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="login-box">
+        <h3>Iniciar sesion</h3>
+        <form action="">
+          <input type="text" name="usuario" id="" />
+          <input type="password" name="contrasena" id="" />
+          <input type="button" value="" />
+        </form>
+      </div>
+    </div>
+  );
+};
